dc0e656b6ee8e768ee4af4e7a381ede7f229279c
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-24 00:15:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_list)
+    #want to compare word_list to word if they match return true if they do not match return empty array
+    word_list.select do |word_2|
+      #take word string - make it into an array - where each array value is a letter of the word ex: word -> ["w", "o", "r", "d"]
+      (word.split('').sort) == (word_2.split('').sort)
+    end
+  end
+    
+end
