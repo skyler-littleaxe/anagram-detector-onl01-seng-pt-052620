7cc01b493a7742707982df91fe215a9e4bc80c7d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-24 00:10:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     #want to compare word_list to word if they match return true if they do not match return empty array
     word_list.select do |word_2|
       #take word string - make it into an array - 
-      (word.split.sort) ==
+      (word.split.sort) == (word_2.split.sort)
     end
   end
     
